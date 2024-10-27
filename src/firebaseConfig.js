49eadd1cb75c6611d0a3a7be25bd871578222b77
Shri-Utkarsh <130675600+Shri-Utkarsh@users.noreply.github.com>
2024-10-27 14:40:11 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf4PQQbyvh-9IJ_8tTyjTTHsqPAjKkW1k",
-  authDomain: "gdg-admin-panel.firebaseapp.com",
-  projectId: "gdg-admin-panel",
-  storageBucket: "gdg-admin-panel.appspot.com",
-  messagingSenderId: "464259159067",
-  appId: "1:464259159067:web:3290861b124a6a980494be"
+  // type your firebaseConfig api key
 };
 
 // Initialize Firebase
