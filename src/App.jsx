36a2b16c84cae9fb0ea1d54f4d0d45d029e174c9
Utkarsh Srivastava <utkarshsrivastava1422@gmail.com>
2024-10-27 14:36:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminPanel from './components/Admin Panel/AdminPanel';
+
+
+export default function App() {
+  return (
+    <div className="container  max-w-[1200px] mx-auto ">
+        <AdminPanel />
+    </div>
+  );
+}
